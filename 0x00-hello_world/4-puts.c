ff3e05659f724777078e2d6700d896776704fef5
mekuriaw <mekuriawmenberu44@gmail.com>
2023-09-09 07:59:45 -0700

    this is put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,14 @@
  * my_function - This is a description
  */
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
