f1c0be54e92d400b89e10e1054fab3211f978e97
mekuriaw <mekuriawmenberu44@gmail.com>
2023-09-09 07:57:16 -0700

    this is puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-/*
- *main -ethry point
- *retun :always 0 (success)
+/**
+ * my_function - This is a description
  */
+#include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
